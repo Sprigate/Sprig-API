e3757c130f522402e240e5a8e3cc0c35b1caed27
frdnryann <frdnryann@gmail.com>
2026-09-10 02:39:50 +0700

    fix(expenses): ada typo di bagian DTO

--- internal/features/expenses/dto.go
+++ internal/features/expenses/dto.go
@@ -15,12 +15,12 @@ type CreateExpenseRequest struct {
 }
 
 type ExpenseResponse struct {
-	ID           uint64            `json:"id"`
-	UserID       uint64            `json:"user_id"`
-	CategoryID   uint64            `json:"category_id"`
-	Amount       uint64            `json:"amount"`
-	Descripttion *string           `json:"description"`
-	ExpenseDate  time.Time         `json:"expense_date"`
-	Type         model.ExpenseType `json:"type"`
-	UpdatedAt    time.Time         `json:"updated_at"`
+	ID          uint64            `json:"id"`
+	UserID      uint64            `json:"user_id"`
+	CategoryID  uint64            `json:"category_id"`
+	Amount      uint64            `json:"amount"`
+	Description *string           `json:"description"`
+	ExpenseDate time.Time         `json:"expense_date"`
+	Type        model.ExpenseType `json:"type"`
+	UpdatedAt   time.Time         `json:"updated_at"`
 }
